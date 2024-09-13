e37b5ffaf4e6a1834a181bb1bf98aa1147ad1c43
Anastasia Maryina <nastiacool55@gmail.com>
2024-09-13 14:35:18 +0300

    Move from react-scripts to Vite

---
+++ src/components/app/app.jsx
@@ -0,0 +1,41 @@
+/* React entities import */
+import { lazy, Suspense } from "react";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+/* Components static import */
+import AppHeader from "../app-header/app-header";
+import Spinner from "../spinner/spinner";
+
+/* Styles import */
+import "./app.scss";
+
+/* Lazy loading of pages (dynamic imports) */
+const NotFoundPage = lazy(() => import("../pages/404"));
+const CharactersPage = lazy(() => import("../pages/characters-page"));
+const ComicsPage = lazy(() => import("../pages/comics-page"));
+const DetailsPage = lazy(() => import("../pages/details-page"));
+
+const App = () => {
+	return (
+		<Router>
+			<div className="app-container">
+				<AppHeader />
+				<main>
+					<Suspense fallback={<Spinner />}>
+						<Routes>
+							<Route path={"/"} element={<CharactersPage />} />
+							<Route path={"/characters"} element={<CharactersPage />} />
+							<Route path={"/comics"} element={<ComicsPage />} />
+							<Route path={"/:type/:id"} element={<DetailsPage />} />
+							<Route path={"/*"} element={<NotFoundPage />} />
+						</Routes>
+					</Suspense>
+				</main>
+				<footer>Data provided by Marvel. © 2014 Marvel</footer>
+			</div>
+		</Router>
+	);
+};
+
+export default App;
